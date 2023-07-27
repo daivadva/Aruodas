8cf1b3c42c16b27c6c978785c49f60e0fc927606
daivadva <daiva.daniuleviciute@gmail.com>
2023-07-27 21:39:02 +0300

    papildyti testai, nauja kategorija

--- src/test/java/PremisesTest.java
+++ src/test/java/PremisesTest.java
@@ -3,7 +3,9 @@ import org.example.Premises;
 import org.example.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -19,6 +21,48 @@ public class PremisesTest {
                 new String[]{AdFeatures.maitinimo,AdFeatures.paslaugu});
         ad.fillAd();
         ad.submit();
+    }
+
+    @Test
+    public void newNoArea(){
+        Premises ad = new Premises("Klaipėda", "Debrecenas", "Naujakiemio g.", "","10000", "+37061234567", "C:\\Users\\Daiva\\Desktop\\DSC_0867.JPG", "5",
+                new String[]{AdFeatures.pamatai},
+                new String[]{AdFeatures.maitinimo,AdFeatures.paslaugu});
+        ad.fillAd();
+        ad.submit();
+        Utils.wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("/html/body/div[1]/div[2]/form/ul/li[15]/span[4]"), "Įveskite bendrą plotą"));
+        Assert.assertEquals(Utils.driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/ul/li[15]/span[4]")).getText(), "Įveskite bendrą plotą");
+    }
+
+    @Test
+    public void newNoFloor(){
+        Premises ad = new Premises("Klaipėda", "Debrecenas", "Naujakiemio g.", "100","10000", "+37061234567", "C:\\Users\\Daiva\\Desktop\\DSC_0867.JPG", "",
+                new String[]{AdFeatures.pamatai},
+                new String[]{AdFeatures.maitinimo,AdFeatures.paslaugu});
+        ad.fillAd();
+        ad.submit();
+        Utils.wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("/html/body/div[1]/div[2]/form/ul/li[16]/div/span[4]"), "Įveskite aukštą"));
+        Assert.assertEquals(Utils.driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/ul/li[16]/div/span[4]")).getText(), "Įveskite aukštą");
+    }
+
+    @Test
+    public void newAdWithAllEquipment(){
+        Premises ad = new Premises("Klaipėda", "Debrecenas", "Naujakiemio g.", "100","10000", "+37061234567", "C:\\Users\\Daiva\\Desktop\\DSC_0867.JPG", "5",
+                new String[]{AdFeatures.irengtas,AdFeatures.dalineApdaila,AdFeatures.neirengtas,AdFeatures.nebaigtasStatyti,AdFeatures.kita,AdFeatures.pamatai},
+                new String[]{AdFeatures.maitinimo,AdFeatures.paslaugu});
+        ad.fillAd();
+        Assert.assertEquals(Utils.driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/ul/li[18]/div/div[5]/div[2]")).getText(), "Pamatai");
+    }
+
+    @Test
+    public void newAdWithNoEquipment(){
+        Premises ad = new Premises("Klaipėda", "Debrecenas", "Naujakiemio g.", "100","10000", "+37061234567", "C:\\Users\\Daiva\\Desktop\\DSC_0867.JPG", "5",
+                new String[]{},
+                new String[]{AdFeatures.maitinimo,AdFeatures.paslaugu});
+        ad.fillAd();
+        ad.submit();
+        Utils.wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("/html/body/div[1]/div[2]/form/ul/li[18]/span"), "Pasirinkite įrengimą"));
+        Assert.assertEquals(Utils.driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/ul/li[18]/span")).getText(), "Pasirinkite įrengimą");
     }
 
 
